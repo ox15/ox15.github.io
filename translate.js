774138d5ae4e1411d253dd6b60d9af0a9c5819ba
Jcad <jcadcell@users.noreply.github.com>
2018-03-17 17:40:24 -0600

    Added a tooltip for the site map. Added a translation div

---
+++ translate.js
@@ -0,0 +1,3 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'es,it,ru,uk', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, multilanguagePage: true}, 'google_translate_element');
+}
