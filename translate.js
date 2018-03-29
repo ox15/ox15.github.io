1bb96c8a166874eb64ace3d097aad552640d23e5
Jcad <jcadcell@users.noreply.github.com>
2018-03-29 17:28:50 -0600

    Added help and how2 folders to the main folder. Moved the site map to its own folder. Moved styles to stylesheet. Links open in the same tab. Added a favicon.

--- translate.js
+++ translate.js
@@ -1,3 +1,3 @@
 function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'es,it,ru,uk', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, multilanguagePage: true}, 'google_translate_element');
+  new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'es,it,ru,uk', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
 }
