4e3f9fa785427040ade812437448a91af0070331
Jcad <jcadcell@users.noreply.github.com>
2020-05-09 18:59:51 -0400

    Added Frogger game with styling. Doesn't do anything

--- games/js/frogger/app.js
+++ games/js/frogger/app.js
@@ -5,7 +5,30 @@ document.addEventListener('DOMContentLoaded', () => {
 	const startBtn = document.querySelector('#button')
 	const width = 9
 	let currentIndex = 76
+	let timerId
+
 
 	squares[currentIndex].classList.add('frog')
+
+	function moveFrog(e) {
+		squares[currentIndex].classList.renove('frog')
+		switch(e.keyCode) {
+			case 37:
+				if(currentIndex % width !== 0) currentIndex -= 1
+				break
+			case 38:
+				if(currentIndex - width >= 0) currentIndex -= width
+				break
+			case 39:
+				if(currentIndex % width < width -1) currentIndex += 1
+				break
+			case 40:
+				if(currentIndex + width < width*width) currentIndex += width
+				break
+		}
+		squares[currentIndex].classList.add('frog')
+		lose()
+		win()
+	}
 	
 })
