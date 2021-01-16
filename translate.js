c7d04c6fce3a898dd60046a8c886270f65adcea4
ox15 <me@samueltimes.com>
2021-01-16 16:42:36 -0500

    Removed Translate widget on all pages

--- translate.js
+++
@@ -1,7 +0,0 @@
-function googleTranslateElementInit() {
-    'use strict';
-    new google.translate.TranslateElement({
-        pageLanguage: 'en',
-        includedLanguages: 'ar,de,es,fa,it,ja,nl,no,ru,uk,ur',
-        layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-}
