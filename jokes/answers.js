2cb086fd0ea8f1054db3bf67629675065c5300d5
ox15 <me@samueltimes.com>
2021-08-21 12:16:06 -0400

    Updated style on joke buttons, added career jokes

--- jokes/answers.js
+++ jokes/answers.js
@@ -1,79 +1,79 @@
 // This script shows the answers to the jokes and fades the buttons when you click them.
 $(document).ready(function(){
-	$("#button2").click(function(){
+	$(".button2").click(function(){
 		$( ".2" ).fadeIn();
-		$("#button2").fadeOut();
+		$(".button2").fadeOut();
         
 
 	});
-    $("#button3").click(function(){
+    $(".button3").click(function(){
 		$(".3").fadeIn();
-		$("#button3").fadeOut();
+		$(".button3").fadeOut();
         
 
 	});
-     $("#button4").click(function(){
+     $(".button4").click(function(){
 		$(".4").fadeIn();
-		$("#button4").fadeOut();
+		$(".button4").fadeOut();
         
 
 	});
-    $("#button5").click(function(){
+    $(".button5").click(function(){
 		$(".5").fadeIn();
-		$("#button5").fadeOut();
+		$(".button5").fadeOut();
         
 
 	});
-    $("#button6").click(function(){
+    $(".button6").click(function(){
 		$(".6").fadeIn();
-		$("#button6").fadeOut();
+		$(".button6").fadeOut();
         
 
 	});
-     $("#button7").click(function(){
+     $(".button7").click(function(){
 		$(".7").fadeIn();
-		$("#button7").fadeOut();
+		$(".button7").fadeOut();
         
 
 	});
-    $("#button8").click(function(){
+    $(".button8").click(function(){
 		$(".8").fadeIn();
-		$("#button8").fadeOut();
+		$(".button8").fadeOut();
         
 
 	});
-    $("#button9").click(function(){
+    $(".button9").click(function(){
 		$(".9").fadeIn();
-		$("#button9").fadeOut();
+		$(".button9").fadeOut();
         
 
 	});
-    $("#button10").click(function(){
+    $(".button10").click(function(){
 		$(".10").fadeIn();
-		$("#button10").fadeOut();
+		$(".button10").fadeOut();
         
 
 	});
-    $("#button11").click(function(){
+    $(".button11").click(function(){
 		$(".11").fadeIn();
-		$("#button11").fadeOut();
+		$(".button11").fadeOut();
         
 
 	});
-    $("#button12").click(function(){
+    $(".button12").click(function(){
 		$(".12").fadeIn();
-		$("#button12").fadeOut();
+		$(".button12").fadeOut();
         
 
 	});
-    $("#buttonA").click(function(){
-        $("#buttonA").fadeOut();
+    $(".buttonA").click(function(){
+        $(".buttonA").fadeOut();
 		$(".A").fadeIn();
         
 
 	});
-    $("#buttonB").click(function(){
-        $("#buttonB").fadeOut();
+    $(".buttonB").click(function(){
+        $(".buttonB").fadeOut();
 		$(".B").fadeIn();
         
 
